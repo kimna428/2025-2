495848a52912c285fd7723b74c1d60e64b1d4a90
kimna428 <nayoung31002@gmail.com>
2025-06-23 15:14:51 +0900

    6/23

--- baekjoon/13015.cpp
+++ baekjoon/13015.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 using namespace std;
 
+void sim(int N){
+    cout << "*";
+    for(int j=0; j<N-2; j++) cout <<" ";
+    cout << "*";    
+}
+
 int main() {
     int N;
     cin >> N;
-    for(int i=0; i<N; i++){
-        cout << "*";
-    }   
+    if(N!=2){
+        for(int i=0; i<2*N-1; i++){
+            if(i == 0|| i== 2*N-2){
+                for(int j=0; j<N; j++)cout << "*";
+                for(int j=0; j<2*N-3; j++)cout << " ";
+                for(int j=0; j<N; j++)cout << "*";
+                cout << "\n";
+            } else if(i<N-1){
+                for(int j = 0; j<i; j++) cout << " ";
+                sim(N);
+                for(int j=0; j<2*N-3-2*i; j++)cout << " ";
+                sim(N);
+                cout << "\n";
+            } else if(i == N-1){
+                for(int j=0; j<N-1; j++) cout<<" ";
+                sim(N);
+                for(int j=0; j<N-2; j++) cout <<" ";
+                cout << "*";
+                cout << "\n";    
+            } else if(i>N-1){
+                for(int j = 2*(N-1); j>i; j--) cout << " ";
+                sim(N);
+                for(int j=0; j<(i-N)*2+1; j++)cout << " ";
+                sim(N);
+                cout << "\n";
+            } 
+        }
+    }else{
+        cout<<"** **\n";
+        cout<<" ***\n";
+        cout<<"** **";
+    }
 }
