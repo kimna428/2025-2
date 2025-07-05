1230daccf6987d6e7613ec867ffcc6649e946d17
kimna428 <nayoung31002@gmail.com>
2025-07-05 23:43:10 +0900

    7/5

--- baekjoon/15651.cc
+++ baekjoon/15651.cc
@@ -1,2 +1,21 @@
 #include <iostream>
-usin namespace std;
+using namespace std;
+
+//4,2
+void func(int n, int m){
+    
+    for(int i=1; i<=n; i++){
+        cout << i << " ";
+        if(m != 1) func(n, m-1);
+        cout << "\n";
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    int N, M;
+    cin >> N >> M;
+    for(int i=0; i<M; i++){
+       func(N, M);
+    }
+}
